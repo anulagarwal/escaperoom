c416e6fd84ee7c0b9912367f9b004bbdaf9577fa
unitedgamedevelopers <krushilgamedev20@gmail.com>
2021-04-06 14:45:30 +0530

    Camera Rotation

---
+++ Assets/Scripts/SupportScripts/CameraMarker.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMarker : MonoBehaviour
+{
+    #region Poperties
+    [Header("Attributes")]
+    [SerializeField] private float rotSpeed = 0f;
+
+    private float width = 0f;
+    #endregion
+
+    #region MonoBehaviour Functions
+    private void Start()
+    {
+        width = (float)Screen.width / 2f;
+    }
+
+    private void Update()
+    {
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (Input.touchCount == 1)
+            {
+                touch = Input.GetTouch(0);
+
+                if (touch.phase == TouchPhase.Moved)
+                {
+                    if (touch.position.x > width)
+                    {
+                        transform.Rotate(Vector3.up * Time.deltaTime * rotSpeed);
+                    }
+                    else
+                    {
+                        transform.Rotate(-Vector3.up * Time.deltaTime * rotSpeed);
+                    }
+                }
+            }
+        }
+    }
+    #endregion
+}
